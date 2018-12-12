e20416a9c373a63ed44c00f59f88651276ba50b5
Michał <M1nified@users.noreply.github.com>
2018-12-12 20:47:41 +0100

    Update #1.js

--- #1.js
+++ #1.js
@@ -1,42 +1,6 @@
 // Problem:
 // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
-function randomIndex(arr, andNot = null) {
-  let value = andNot;
-  while ((value = Math.floor(Math.random() * (arr.length - 1))) == andNot);
-  return value;
-}
-
-const testNumbers = Array(20).fill(0).map(_ => Math.floor(Math.random() * 1000)),
-  r1 = randomIndex(testNumbers),
-  r2 = randomIndex(testNumbers, r1),
-  r3 = randomIndex(testNumbers),
-  r4 = randomIndex(testNumbers, r3),
-  k1 = testNumbers[r1] + testNumbers[r2],
-  k2 = testNumbers[r3] + testNumbers[r4];
-
-function testWith(fun) {
-  const
-    resultTrue1 = fun(testNumbers, k1),
-    resultTrue2 = fun(testNumbers, k2),
-    resultFalse1 = fun(testNumbers, -1)
-  if (resultTrue1 === true) {
-    console.log(`OK: sum ${k1} in [${testNumbers}]`);
-  } else {
-    console.log(`FAILED: sum ${k1} in [${testNumbers}]`);
-  }
-  if (resultTrue2 === true) {
-    console.log(`OK: sum ${k2} in [${testNumbers}]`);
-  } else {
-    console.log(`FAILED: sum ${k2} in [${testNumbers}]`);
-  }
-  if (resultFalse1 === false) {
-    console.log(`OK: sum -1 not in [${testNumbers}]`);
-  } else {
-    console.log(`FAILED: sum -1 not in [${testNumbers}]`);
-  }
-}
-
 function solution1(numbers, k) {
   for (let i = 0; i < numbers.length; i++) {
     const desiredValue = k - numbers[i];
@@ -76,6 +40,42 @@ function solution3(numbers, k) {
     map[number] = parseInt(index);
   }
   return false;
+}
+
+function randomIndex(arr, andNot = null) {
+  let value = andNot;
+  while ((value = Math.floor(Math.random() * (arr.length - 1))) == andNot);
+  return value;
+}
+
+const testNumbers = Array(20).fill(0).map(_ => Math.floor(Math.random() * 1000)),
+  r1 = randomIndex(testNumbers),
+  r2 = randomIndex(testNumbers, r1),
+  r3 = randomIndex(testNumbers),
+  r4 = randomIndex(testNumbers, r3),
+  k1 = testNumbers[r1] + testNumbers[r2],
+  k2 = testNumbers[r3] + testNumbers[r4];
+
+function testWith(fun) {
+  const
+    resultTrue1 = fun(testNumbers, k1),
+    resultTrue2 = fun(testNumbers, k2),
+    resultFalse1 = fun(testNumbers, -1)
+  if (resultTrue1 === true) {
+    console.log(`OK: sum ${k1} in [${testNumbers}]`);
+  } else {
+    console.log(`FAILED: sum ${k1} in [${testNumbers}]`);
+  }
+  if (resultTrue2 === true) {
+    console.log(`OK: sum ${k2} in [${testNumbers}]`);
+  } else {
+    console.log(`FAILED: sum ${k2} in [${testNumbers}]`);
+  }
+  if (resultFalse1 === false) {
+    console.log(`OK: sum -1 not in [${testNumbers}]`);
+  } else {
+    console.log(`FAILED: sum -1 not in [${testNumbers}]`);
+  }
 }
 
 console.log(`${k1} = ${testNumbers[r1]}[${r1}] + ${testNumbers[r2]}[${r2}]`);
