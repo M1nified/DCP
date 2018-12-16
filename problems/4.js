8f5c3d403503fe743d2006d0995996c878c04f27
m1nified <m1nified.dev@gmail.com>
2018-12-16 11:50:43 +0100

    + solution 4

---
+++ problems/4.js
@@ -0,0 +1,35 @@
+/**
+Problem: Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+**/
+
+function solution1(numbers) {
+    const
+      map = {};
+    let
+      minPositive = null;
+    numbers.forEach(number => {
+      map[number] = true;
+      if (number > 0 && (minPositive === null || number < minPositive))
+        minPositive = number;
+    });
+    let result = minPositive - 1;
+    while (map[++result]);
+    return result;
+  }
+  
+  const
+    in1 = [3, 4, -1, 1],
+    out1 = 3,
+    in2 = [1, 2, 0],
+    out2 = 3;
+  
+  function testUsing(solution) {
+    const
+      result1 = solution(in1),
+      result2 = solution(in2);
+    console.log(`${solution.name}: [${in1}] -> ${result1}`);
+    console.log(`${solution.name}: [${in2}] -> ${result2}`);
+  }
+  
+  testUsing(solution1);
+  
