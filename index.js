d72000b486cea7054e288231f597ac2aa914b6bd
m1nified <m1nified.dev@gmail.com>
2018-12-16 16:56:04 +0100

    changed to single item view

--- index.js
+++ index.js
@@ -1,58 +1,85 @@
-function launchProblem() {
-    const hash = window.location.hash.replace(/^#/, ''),
-        fileName = (`${hash}`),
-        fileUrl = `./${fileName}`;
-    console.debug(fileUrl)
-
-    if (hash == '') {
-        return false;
+"use strict";
+function hashNavigator() {
+    const
+        readme = document.querySelector('.readme'),
+        problem = document.querySelector('.problem');
+    function displayIndex() {
+        problem.classList.add('hidden');
+        readme.classList.remove('hidden');
     }
+    return new Promise((resolve, reject) => {
+        const hash = window.location.hash.replace(/^#/, ''),
+            fileName = (`${hash}`),
+            fileUrl = `./${fileName}`;
 
-    fetch(fileUrl)
-        .then(resp => {
-            if (resp.status === 200)
-                return resp.text()
-        })
-        .then(code => {
-            const codeElement = document.querySelector('.code'),
-                consoleHolder = document.querySelector('.console-holder'),
-                consoleElement = document.createElement('iframe'),
-                codeDetailsElement = document.querySelector('.code-box-header-details');
-            codeElement.innerHTML = '';
-            let codeHtml = Prism.highlight(code, Prism.languages.javascript, 'javascript');
-            // codeElement.appendChild(document.createTextNode(code));
-            codeDetailsElement.innerHTML = `<a href="${fileUrl}">Raw</a>`;
-            codeElement.innerHTML = codeHtml;
-            const
-                html = `<head><style>
-                body{
-                    font-family: monospace;
-                    color: #e7e7e7;
-                }
-                </style></head><body>
-                <pre id="code"></pre>
-                <script>
-                let line = 1;
-                const cc = Object.assign({}, console);
-                console.log = function(...args){
-                    document.getElementById('code').appendChild(document.createTextNode((line++).toString().padStart(3, ' ') + '| ' + args.join(' ') + '\\n')); cc.log(...args);
-                }
-                console.info = function(...args){
-                    document.getElementById('code').appendChild(document.createTextNode((line++).toString().padStart(3, ' ') + '| ' + args.join(' ') + '\\n')); cc.info(...args);
-                }
-                </script>
-                <script>${code}</script></body>`;
-            consoleElement.classList.add('console');
-            consoleElement.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
-            consoleHolder.firstChild && consoleHolder.removeChild(consoleHolder.firstChild);
-            consoleHolder.appendChild(consoleElement);
-        })
-        .catch(reason => {
-            console.error(reason);
-        })
+        if (hash == '') {
+            displayIndex();
+            resolve();
+            return;
+        }
+
+        launchProblem(fileUrl)
+            .then(() => {
+                problem.classList.remove('hidden');
+                readme.classList.add('hidden');
+            })
+            .catch(reason => {
+                console.error(reason);
+                displayIndex()
+            })
+    })
 }
 
-function launcheReadme() {
+function launchProblem(fileUrl) {
+    return new Promise((resolve, reject) => {
+        fetch(fileUrl)
+            .then(resp => {
+                if (resp.status === 200)
+                    return resp.text()
+                return new Promise((_, reject) => { reject(resp.status) });
+            })
+            .then(code => {
+                const codeElement = document.querySelector('.code'),
+                    consoleHolder = document.querySelector('.console-holder'),
+                    consoleElement = document.createElement('iframe'),
+                    codeDetailsElement = document.querySelector('.code-box-header-details');
+                codeElement.innerHTML = '';
+                let codeHtml = Prism.highlight(code, Prism.languages.javascript, 'javascript');
+                // codeElement.appendChild(document.createTextNode(code));
+                codeDetailsElement.innerHTML = `<a href="${fileUrl}">Raw</a>`;
+                codeElement.innerHTML = codeHtml;
+                const
+                    html = `<head><style>
+            body{
+                font-family: monospace;
+                color: #e7e7e7;
+            }
+            </style></head><body>
+            <pre id="code"></pre>
+            <script>
+            let line = 1;
+            const cc = Object.assign({}, console);
+            console.log = function(...args){
+                document.getElementById('code').appendChild(document.createTextNode((line++).toString().padStart(3, ' ') + '| ' + args.join(' ') + '\\n')); cc.log(...args);
+            }
+            console.info = function(...args){
+                document.getElementById('code').appendChild(document.createTextNode((line++).toString().padStart(3, ' ') + '| ' + args.join(' ') + '\\n')); cc.info(...args);
+            }
+            </script>
+            <script>${code}</script></body>`;
+                consoleElement.classList.add('console');
+                consoleElement.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+                consoleHolder.firstChild && consoleHolder.removeChild(consoleHolder.firstChild);
+                consoleHolder.appendChild(consoleElement);
+                resolve();
+            })
+            .catch(reason => {
+                reject(reason);
+            })
+    })
+}
+
+function launchReadme() {
     const readmeUrl = `./README.md`;
     fetch(readmeUrl)
         .then(resp => {
@@ -69,8 +96,17 @@ function launcheReadme() {
         .catch(reason => console.error(reason))
 }
 
-launchProblem();
+hashNavigator();
 window.addEventListener("hashchange", () => {
-    launchProblem();
+    hashNavigator();
 }, false);
-launcheReadme();
+launchReadme();
+
+document.querySelectorAll('.link-home').forEach(link => {
+    const goHome = event => {
+        event.preventDefault();
+        window.location.hash = '';
+    }
+    link.addEventListener('click', goHome);
+})
+
